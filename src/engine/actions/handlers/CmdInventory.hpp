7b064b7a4d5df578b3dedab22e1632a12529065b
Antoine <me@antoinebellanger.fr>
2025-06-26 16:48:07 +0200

    fix: compilation error
    Signed-off-by: Antoine <me@antoinebellanger.fr>

--- src/engine/actions/handlers/CmdInventory.hpp
+++ src/engine/actions/handlers/CmdInventory.hpp
@@ -23,6 +23,6 @@ namespace zappy::engine::cmd
     class CmdInventory
     {
         public:
-            static void cmdInventory(std::weak_ptr<Player> player, World& world, const std::string& args)
+            static void cmdInventory(std::weak_ptr<Player> player, World& world, const std::string& args);
     };
 }
